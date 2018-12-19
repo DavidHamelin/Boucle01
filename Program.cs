e2fd4c8cdc9630c86a5f79c8b21a8acfabea7b87
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 13:29:30 +0100

    Boucle 01

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucle01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while ( i < 10)
+            {
+                Console.WriteLine("Bonjour, je suis un simple message");
+                i++;
+            }
+        }
+    }
+}
